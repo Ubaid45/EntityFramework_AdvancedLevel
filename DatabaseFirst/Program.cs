b5dd99bf0d742c084e56c27e0c25281285f49543
Ubaid Rana <Ubaid Rana@CALLMEBT>
2020-04-04 12:46:18 +0200

    Initial Commit (Database First Approach)

---
+++ DatabaseFirst/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace DatabaseFirst
+{
+    class Program
+    {
+   
+        static void Main()
+        {
+            var dbContext = new PlutoDbContext();
+            var courses = dbContext.GetCourses().ToList();
+            foreach (var course in courses)
+            {
+                Console.WriteLine(course.Title);
+            }
+
+            while (true)
+            {
+                
+            }
+        }
+    }
+}
